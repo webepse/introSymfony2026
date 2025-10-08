449651f0a9f1157cff817105d2b5502a32b5e437
Boderio <berti@epse.be>
2025-10-08 11:47:21 +0200

    création d'une fixtures pour la base de données dans la table Product

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,18 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        for($cpt=1; $cpt<=10; $cpt++)
+        {
+            $product = new Product();
+            $product->setTitle('Product '.$cpt)
+                ->setDescription('Description lorem ipsum')
+                ->setPrice(rand(10,1000))
+                ->setCreateAt(new \DateTimeImmutable())
+                ->setImage('https://picsum.photos/200/300');
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
